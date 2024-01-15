35ad8e1c1a2ea2a80bdf2330cc1a8613fda29e1c
Faizanahmed Saiyed <119733178+Faizanahmedsy@users.noreply.github.com>
2024-01-15 18:45:18 +0530

    added a input feature

--- game-1.py
+++ game-1.py
@@ -10,6 +10,26 @@ def roll():
     return roll;
 
 
-value = roll()
 
-print(value)
+
+
+while True:
+    players = input("Enter the number of players (2-4): ")
+    # isdigit is used to check weather the input is a number or not 
+    if players.isdigit(): 
+        # this will convert the string (which is a digit) in to a integer data type
+        players = int(players) 
+        if 2 <= players <= 4:
+            break
+        else:
+            print("Must be between 2 to 4 players")
+
+
+    else: 
+        print("Input must be a number, please try again")
+
+# print(players)
+# value = roll()
+# print(value)
+
+
