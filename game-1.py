3523f8524fb52a1a863a8ff2fc9adf97851dc77d
Faizanahmed Saiyed <119733178+Faizanahmedsy@users.noreply.github.com>
2024-01-15 19:12:56 +0530

    project completed

--- game-1.py
+++ game-1.py
@@ -28,8 +28,43 @@ while True:
     else: 
         print("Input must be a number, please try again")
 
-# print(players)
-# value = roll()
-# print(value)
 
 
+max_score = 50;
+# This list will store the scores of players
+player_scores = [0 for _ in range(players)]
+
+
+while max(player_scores) < max_score: 
+
+
+    for player_idx in range(players):
+        print("\nPlayer number", player_idx +  1, "turn has just started!")
+        print("Your total score is:", player_scores[player_idx], "\n")
+        current_score = 0
+
+        while True:
+        
+            should_roll = input("Would you like to roll (y)?")
+            if should_roll.lower() != "y":
+                break
+            value = roll()
+            if value == 1:
+                print("Your value is 1, Turn Done!")
+                current_score = 0
+                break
+            else: 
+                current_score += value
+                print("You rolled a", value)
+            
+        
+            print("Your score is:", current_score)
+
+        player_scores[player_idx] += current_score
+        print("Your total score is:" , player_scores[player_idx])
+
+
+
+max_score = max(player_scores)
+winning_idx = player_scores.index(max_score)
+print("Player number", winning_idx + 1 , "is the winner with the score of:", max_score)
